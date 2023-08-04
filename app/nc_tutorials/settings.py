151bc0f5bc1c4ec444c02f2684058025685a4494
primecoach619 <primecoach619@gmail.com>
2023-08-03 20:38:55 -0700

    updating config-new URI, endpoints for week2 8-5-23

--- app/nc_tutorials/settings.py
+++ app/nc_tutorials/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-(j-#aqq@vs$3l#0(031z4_h(_*pa&ye#dt&x69_@(&a6(=+fpg
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    'ec2-3-128-16-74.us-east-2.compute.amazonaws.com', '0.0.0.0', 'localhost', '127.0.0.1'
+    'ec2-34-196-22-195.compute-1.amazonaws.com', '0.0.0.0', 'localhost', '127.0.0.1'
 ]
 
 
